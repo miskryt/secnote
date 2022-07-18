701015c4d9b42cf92e5f85b46b8521c0fb26c434
Mikhail Iskrytski <miskrytsi@solvd.com>
2022-07-18 21:58:42 +0300

    Server.js refactoring

--- src/server.js
+++ src/server.js
@@ -2,27 +2,21 @@
 require('dotenv').config();
 
 const express = require('express');
+const fs = require('fs');
+const http = require('http');
+const Worker = require('./app/worker');
+
 const {TwingEnvironment, TwingLoaderFilesystem} = require('twing');
 let loader = new TwingLoaderFilesystem('./views');
 let twing = new TwingEnvironment(loader);
-const fs = require('fs');
-const https = require('https');
 
-const Worker = require('./app/worker');
+
 const worker = new Worker();
-
 const app = express();
 
-const options = {
-    /*
-    * Используем синхронное чтение, чтобы сертификат точно был загружен до старта сервера
-    * */
-    key: fs.readFileSync("ssl/key.pem"),
-    cert: fs.readFileSync("ssl/cert.pem"),
+const options = {};
 
-};
-
-https.createServer(options, app).listen(process.env.PORT | 3000);
+http.createServer(options, app).listen(process.env.PORT | 3000);
 
 app.set('view engine', 'twing');
 app.set('views', './views')
@@ -31,12 +25,14 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const appname = require('../package.json').name;
-const renderOptions = {'appname': appname, 'result':''};
+const appname = 'Secnote - stay secure'
 
-app.get('/', function(req, res) {
-    renderOptions.result = '';
-    twing.render('pages/home.twig', renderOptions).then((output) => {
+app.get('/', function(req, res)
+{
+    const renderOptions = {'appname': appname, 'result':''};
+
+    twing.render('pages/home.twig', renderOptions).then((output) =>
+    {
         res.end(output);
     });
 });
@@ -48,7 +44,8 @@ app.post('/', async (req, res) => {
 
     const result = await worker.MakeNote(text);
     const url = [baseurl, result].join('/');
-    renderOptions.result = url;
+
+    const renderOptions = {'appname': appname, 'result': url};
 
     twing.render('pages/home.twig', renderOptions).then((output) => {
         res.end(output);
@@ -58,11 +55,12 @@ app.post('/', async (req, res) => {
 app.get('/:hash/:secret', async (req, res) =>
 {
     const baseurl = req.protocol + '://' + req.get('host');
-    renderOptions.baseurl = baseurl;
 
-    renderOptions.action = [baseurl, req.params.hash, req.params.secret].join('/');
+    const action = [baseurl, req.params.hash, req.params.secret].join('/');
+    const renderOptions = {'appname': appname, 'action': action, 'baseurl': baseurl};
 
-    twing.render('pages/decrypt-1.twig', renderOptions).then((output) => {
+    twing.render('pages/decrypt-1.twig', renderOptions).then((output) =>
+    {
         res.end(output);
     });
 
@@ -71,13 +69,13 @@ app.get('/:hash/:secret', async (req, res) =>
 app.post('/:hash/:secret', async (req, res) =>
 {
     const baseurl = req.protocol + '://' + req.get('host');
-    renderOptions.baseurl = baseurl;
-
     const result = await worker.ReadNote(req.params.hash, req.params.secret);
     await worker.DeleteNote(req.params.hash);
-    renderOptions.result = result;
 
-    twing.render('pages/decrypt-2.twig', renderOptions).then((output) => {
+    const renderOptions = {'appname': appname, 'baseurl': baseurl, 'result': result};
+
+    twing.render('pages/decrypt-2.twig', renderOptions).then((output) =>
+    {
         res.end(output);
     });
 });
